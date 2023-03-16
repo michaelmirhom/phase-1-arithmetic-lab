8e2a55d6a485fcee9c87f0a42550d073a06e22f7
michaelmirhom <micharby73@gmail.com>
2023-03-16 13:39:14 -0500

    complteted

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
-
-// Write your code here
+const num1 = 10;
+const num2 = 6.2;
+const multiply = num1 * num2;
+const random = Math.ceil(Math.random() * 100);
+const num3 = 44;
+const num4= 10;
+const mod = num3%num4;
+const set = [5, 10, 20, 15, 8]; // Set of numbers
+const max = Math.max(...set);
